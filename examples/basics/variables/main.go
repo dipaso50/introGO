5a7ce1221f8d5c6b8ab91dba52f9b29c8c450f39
Diego <dipaso50@gmail.com>
2021-04-11 21:25:38 +0200

    mejoras

--- examples/basics/variables/main.go
+++ examples/basics/variables/main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 
+	//declaración de variables
+	//----------------------------------------------------------------------------------------
 	var a = "initial"
 	fmt.Println(a)
 
@@ -18,4 +20,38 @@ func main() {
 
 	f := "apple"
 	fmt.Println(f)
+
+	//constantes
+	//----------------------------------------------------------------------------------------
+
+	const Pi = 3.14
+	fmt.Println(Pi)
+
+	//arrays
+	//----------------------------------------------------------------------------------------
+	var arr [4]int //Slice sin tamaño definido
+	arr[0] = 33
+	fmt.Printf("arr --> len(%d) cap(%d)\n", len(arr), cap(arr))
+
+	//slices
+	//----------------------------------------------------------------------------------------
+	var slcs []int //Slice sin tamaño definido
+	slcs = append(slcs, 1, 2, 3)
+	fmt.Printf("slcs --> len(%d) cap(%d) %v\n", len(slcs), cap(slcs), slcs)
+
+	var slcs2 []int           //Declaración de slice
+	slcs2 = make([]int, 3, 3) //Inicialización con 3 elementos y capacidad de 3
+	fmt.Printf("slcs2 --> len(%d) cap(%d)\n", len(slcs2), cap(slcs2))
+
+	//maps
+	//----------------------------------------------------------------------------------------
+
+	var m map[string]int     //declaracion
+	m = make(map[string]int) //inicialización
+	delete(m, "nothing")     // delete a element
+
+	//uso
+	m["route"] = 66
+	i := m["route"]
+	fmt.Printf("m --> len(%d) %d\n", len(m), i)
 }
